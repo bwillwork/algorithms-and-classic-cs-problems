03d249fe67ccc8b544354f63ab44ac3f89773b62
Bryan Williams <bwillwork@proton.me>
2026-04-18 13:08:30 -0400

    First commit

---
+++ src/main/java/com/example/acsp/AlgorithmsAndClassicCsProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.acsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsAndClassicCsProblemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsAndClassicCsProblemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/acsp/AlgorithmsAndClassicCsProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsAndClassicCsProblemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
